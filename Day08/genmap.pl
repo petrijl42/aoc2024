447c1f1f97987db22e5dd1aa15fa31ca0842ee0f
Petri Lahtinen <pjl@starfleet.fi>
2024-12-08 12:40:10 +0200

    Fix width and height being swapped Check for overlapping antennas

--- Day08/genmap.pl
+++ Day08/genmap.pl
@@ -10,27 +10,30 @@ my $config = {
     }
 };
 
-for (my $x = 0; $x < $config->{width}; $x++)
+for (my $y = 0; $y < $config->{height}; $y++)
 {
-    for (my $y = 0; $y < $config->{height}; $y++)
+    for (my $x = 0; $x < $config->{width}; $x++)
     {
+        my $found = 0;
+
         foreach my $antenna (keys %{$config->{antennas}})
         {
-            my $found = 0;
             foreach my $pos (@{$config->{antennas}->{$antenna}})
             {
                 if ($pos->[0] == $x && $pos->[1] == $y)
                 {
+                    die "Antenna $antenna is placed on top of another antenna\n" if $found;
+
                     $found = 1;
                     print $antenna;
                     last;
                 }
             }
-            if (not $found)
-            {
-                print ".";
-                next;
-            }
+        }
+        if (not $found)
+        {
+            print ".";
+            next;
         }
     }
     print "\n";
