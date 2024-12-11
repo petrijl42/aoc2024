5a1af7be7509c6834a5f0f9ebbcd9c9159ab38f2
Petri Lahtinen <pjl@starfleet.fi>
2024-12-12 00:11:35 +0200

    Comment out max finding. Remove useless $string.

--- Day11/program3.pl
+++ Day11/program3.pl
@@ -6,7 +6,7 @@ my $fd;
 
 open($fd, "<", "input2.txt") or die "Could not open input for reading";
 my $iterations = 75;
-my $max = 0;
+# my $max = 0;
 
 my $line = <$fd> || die "Could not read from input";
 
@@ -31,7 +31,7 @@ foreach my $stone (@$stones)
 }
 
 print "Result: $count\n";
-print "Max: " . $max ."\n";
+# print "Max: " . $max ."\n";
 
 sub stone_count
 {
@@ -41,14 +41,13 @@ sub stone_count
 
     $iterations--;
 
-    if ($stone > $max)
-    {
-        $max = $stone;
-    }
+    # if ($stone > $max)
+    # {
+    #     $max = $stone;
+    # }
 
     if ($iterations < 0)
     {
-        # print $stone->bstr() . " ";
         return 1;
     }
 
@@ -57,22 +56,21 @@ sub stone_count
         return stone_count(1, $iterations, $cache);
     }
 
-    my $string = $stone;
-    my $length = length($string);
+    my $length = length($stone);
 
     if ($length % 2 == 0)
     {
-        if (not defined $cache->{$string}->{$iterations})
+        if (not defined $cache->{$stone}->{$iterations})
         {
             my $half = $length / 2;
 
-            my $count = stone_count(int(substr($string, 0, $half)), $iterations, $cache) +
-                        stone_count(int(substr($string, $half, $half)), $iterations, $cache);
+            my $count = stone_count(int(substr($stone, 0, $half)), $iterations, $cache) +
+                        stone_count(int(substr($stone, $half, $half)), $iterations, $cache);
 
-            $cache->{$string}->{$iterations} = $count;
+            $cache->{$stone}->{$iterations} = $count;
         }
 
-        return $cache->{$string}->{$iterations};
+        return $cache->{$stone}->{$iterations};
     }
 
     return stone_count($stone * 2024, $iterations, $cache);
