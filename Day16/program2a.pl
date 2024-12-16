8383298558954f1b0a406b6b81bd32ce1593d582
Petri Lahtinen <pjl@starfleet.fi>
2024-12-16 19:16:37 +0200

    Optimized version

---
+++ Day16/program2a.pl
@@ -0,0 +1,247 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Switch;
+
+main ();
+
+sub main
+{
+    my $maze = read_maze("input2.txt");
+    my $score = walk_maze($maze);
+
+    print "Score: $score\n";
+}
+
+sub read_maze
+{
+    my $filename = shift;
+    my $fd;
+    open($fd, "<", $filename) or die "Could not open input for reading";
+
+    my $start;
+    my $map = [];
+
+    while (my $line = <$fd>)
+    {
+        chomp $line;
+
+        if ((my $x = index($line, "S")) != -1)
+        {
+            die "Invalid map, multiple starting points\n" if (defined $start);
+
+            $start = { x => $x, y => scalar(@{$map}) };
+        }
+
+        push(@$map, [split(//, $line)]);
+    }
+
+    close($fd);
+
+    return { map => $map, start => $start, direction => { x => 1, y => 0 } };
+}
+
+sub walk_maze
+{
+    my $maze = shift;
+
+    my $position = $maze->{start};
+    my $direction = $maze->{direction};
+    my $map = $maze->{map};
+
+    my $score;
+    my $visited = {};
+    my $best_path = {};
+    my $history = [ { x => $position->{x}, y => $position->{y} } ];
+
+    my @priority = ({ position => $position, direction => $direction, score => 0, history => [@{$history}] });
+
+    my @paths = ({ position => $position, direction => turn_counter_clockwise($direction), score => 1000, history => [@{$history}] },
+                 { position => $position, direction => turn_clockwise($direction), score => 1000, history => [@{$history}] });
+
+    while (@paths > 0 or @priority > 0)
+    {
+        my $path;
+
+        if (@priority > 0)
+        {
+            $path = shift @priority;
+        }
+        else
+        {
+            $path = shift @paths;
+        }
+
+        my $x = $path->{position}->{x} + $path->{direction}->{x};
+        my $y = $path->{position}->{y} + $path->{direction}->{y};
+
+        if ($y < 0 or $y >= scalar(@$map) or $x < 0 or $x >= scalar(@{$map->[$y]}))
+        {
+            die "Exited map without hitting a wall\n";
+        }
+
+        $path->{score}++;
+
+        next if ((defined $score) && ($path->{score} > $score));
+
+        next if ((defined $visited->{$x}->{$y}) && (($visited->{$x}->{$y} + 1000) < ($path->{score})));
+
+        $visited->{$x}->{$y} = $path->{score};
+
+        push @{$path->{history}}, { x => $x, y => $y };
+
+        switch ($map->[$y]->[$x])
+        {
+            case "#"
+            {
+            }
+            case "S"
+            {
+            }
+            case "."
+            {
+                my $direction;
+
+                $direction = turn_counter_clockwise($path->{direction});
+                if (is_valid_position($map, { x => $x, y => $y }, $direction))
+                {
+                    push @paths,
+                    {
+                        position => { x => $x, y => $y },
+                        direction => $direction,
+                        score => $path->{score} + 1000,
+                        history => [@{$path->{history}}]
+                    };
+                }
+
+                $direction = turn_clockwise($path->{direction});
+                if (is_valid_position($map, { x => $x, y => $y }, $direction))
+                {
+                    push @paths,
+                    {
+                        position => { x => $x, y => $y },
+                        direction => $direction,
+                        score => $path->{score} + 1000,
+                        history => [@{$path->{history}}]
+                    };
+                }
+
+                $direction = $path->{direction};
+                if (is_valid_position($map, { x => $x, y => $y }, $direction))
+                {
+                    push @priority,
+                    {
+                        position => { x => $x, y => $y },
+                        direction => $direction,
+                        score => $path->{score},
+                        history => [@{$path->{history}}]
+                    };
+                }
+            }
+            case "E"
+            {
+                # print "Score: $path->{score}\n";
+                if (not defined $score)
+                {
+                    $score = $path->{score};
+                }
+                if ($path->{score} < $score)
+                {
+                    $score = $path->{score};
+                    $best_path = {};
+                }
+                if ($path->{score} == $score)
+                {
+                    foreach my $point (@{$path->{history}})
+                    {
+                        my $point_str = "$point->{x},$point->{y}";
+                        $best_path->{$point_str} = 1;
+                    }
+                }
+            }
+            else
+            {
+                die "Invalid character $map->[$y]->[$x] in map\n";
+            }
+        }
+    }
+
+    # print_map($map, $best_path);
+
+    return int(keys %$best_path);
+}
+
+sub is_valid_position
+{
+    my $map = shift;
+    my $position = shift;
+    my $direction = shift;
+
+    my $x = $position->{x} + $direction->{x};
+    my $y = $position->{y} + $direction->{y};
+
+    switch ($map->[$y]->[$x])
+    {
+        case "#"
+        {
+            return 0;
+        }
+        case "S"
+        {
+            return 0;
+        }
+        case "."
+        {
+            return 1;
+        }
+        case "E"
+        {
+            return 1;
+        }
+        else
+        {
+            die "Invalid character $map->[$y]->[$x] in map\n";
+        }
+    }
+}
+
+sub turn_clockwise
+{
+    my $direction = shift;
+
+    my $new_direction = { x => $direction->{y} * - 1, y => $direction->{x} };
+
+    return $new_direction;
+}
+
+sub turn_counter_clockwise
+{
+    my $direction = shift;
+
+    my $new_direction = { x => $direction->{y}, y => $direction->{x} * -1 };
+
+    return $new_direction;
+}
+
+sub print_map
+{
+    my $map = shift;
+    my $best_path = shift;
+
+    for (my $y = 0; $y < int(@{$map}); $y++)
+    {
+        for (my $x = 0; $x < int(@{$map->[$y]}); $x++)
+        {
+            my $point_str = "$x,$y";
+            if (defined $best_path->{$point_str})
+            {
+                print "O";
+            }
+            else
+            {
+                print $map->[$y]->[$x];
+            }
+        }
+        print "\n";
+    }
+}
